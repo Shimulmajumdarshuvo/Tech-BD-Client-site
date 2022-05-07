84904626599c7906000ebb83af938f5672badae0
shimul <shimul@gmail.com>
2022-05-07 08:49:20 +0600

    site updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AllInventory from './Card/AllInventory/AllInventory';
+import InventoryCard from './Card/InventoryCard/InventoryCard';
 import Blog from './Components/Blog/Blog';
 import Home from './Components/Home/Home';
 import Inventory from './Components/Home/Inventory/Inventory';
@@ -22,6 +23,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/service/:serviceId' element={<StokeUpdate></StokeUpdate>}></Route>
+        <Route path='service/:id' element={<InventoryCard></InventoryCard>}></Route>
         <Route path='allinventory' element={<AllInventory></AllInventory>}></Route>
         <Route path='blog' element={<Blog></Blog>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
--- src/Card/InventoryCard/InventoryCard.js
+++ src/Card/InventoryCard/InventoryCard.js
@@ -2,7 +2,24 @@ import React from 'react';
 import './InventoryCard.css';
 
 const InventoryCard = (props) => {
-    const { name, img, review, price, quantity } = props.product;
+    const { _id, name, img, review, price, quantity } = props.product;
+
+
+    const handleUserDelete = id => {
+        const proceed = window.confirm('Are you sure want to detete? ');
+        if (proceed) {
+            console.log('hfjdhjdh', id);
+            const url = `http://localhost:5000/service/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                })
+        }
+
+    }
     return (
         <div>
             <div className='card-component'>
@@ -13,7 +30,7 @@ const InventoryCard = (props) => {
                 <p> Price: $ {price}</p>
                 <p> Quantity : {quantity}</p>
 
-                <button>Delete</button>
+                <button onClick={() => handleUserDelete(_id)}>Delete</button>
 
             </div>
 
